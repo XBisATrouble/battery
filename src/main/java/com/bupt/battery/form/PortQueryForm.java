7bd28cb33b075a84fab1eddfabcc2f17020bf6f0
“xbisatrouble” <xbisatrouble@gmail.com>
2019-12-05 23:00:46 +0800

    增加增改删接口

--- src/main/java/com/bupt/battery/form/PortQueryForm.java
+++ src/main/java/com/bupt/battery/form/PortQueryForm.java
@@ -4,6 +4,10 @@ import lombok.Data;
 
 @Data
 public class PortQueryForm {
+    // 用于查询Port
+    private Long id;
+    private Integer pageNum;
+    private Integer pageSize;
     private String portName;
     private Integer status;
 }
--- src/main/java/com/bupt/battery/service/IPortDOService.java
+++ src/main/java/com/bupt/battery/service/IPortDOService.java
@@ -3,12 +3,12 @@ package com.bupt.battery.service;
 import com.bupt.battery.entity.ModelDO;
 import com.bupt.battery.entity.PortDO;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import javax.sound.sampled.Port;
 import java.util.List;
 
 public interface IPortDOService extends BaseService<PortDO,Long> {
-    PortDO findByName(String portName);
     PortDO findByNameAndStatus(String portName,Integer status);
-    Page<PortDO> findPortPage();
+    Page<PortDO> findPortPage(Pageable pageable);
 }
