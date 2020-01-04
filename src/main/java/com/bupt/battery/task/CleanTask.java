9d41fdb944141bb7cd5384544e5db92e5d471fba
ics091 <icsrighttrack@outlook.com>
2020-01-04 17:04:09 +0800

    merge

--- src/main/java/com/bupt/battery/task/CleanTask.java
+++ src/main/java/com/bupt/battery/task/CleanTask.java
@@ -1,5 +1,6 @@
 package com.bupt.battery.task;
 
+import com.alibaba.fastjson.JSONObject;
 import com.bupt.battery.Enum.StatusType;
 import com.bupt.battery.config.WebSocket;
 import com.bupt.battery.entity.TaskDO;
@@ -16,13 +17,10 @@ public class CleanTask{
         System.out.println("request:-------"+request);
         try {
             System.out.println("start");
-
             String[] args1 = new String[] {
-                "python",
-                //                "/home/python/tmat.py",
-                //"/home/python/tmat.py",
-                    "D:\\model\\Clean\\pre.py",
-                request
+                    "python",
+                    "D:\\model\\Clean\\pre1.py",
+                    request
             };
             Process pr = Runtime.getRuntime().exec(args1);
             InputStreamReader ir = new InputStreamReader(pr.getInputStream());
