6cc38c6663b0ca17b3ef383719643b9b9c042a63
XBisATrouble <XBisATrouble@gmail.com>
2020-05-30 11:33:33 +0800

    修改调起python文件的命令

--- src/main/java/com/bupt/battery/task/CallMonitorThread.java
+++ src/main/java/com/bupt/battery/task/CallMonitorThread.java
@@ -1,6 +1,8 @@
 package com.bupt.battery.task;
 
 
+import java.util.HashMap;
+
 /**
  * 处理Socket请求的线程类
  */
@@ -17,8 +19,13 @@ public class CallMonitorThread implements Runnable {
     @Override
     public void run() {
         try {
-            //String command = "python D:\\Workshop\\untitled4\\Model1.py " + port;
-            String command = "python /home/python/early_warning.py " + port +" hot";
+            HashMap<Integer, String> typeMap = new HashMap<Integer, String>();
+            typeMap.put(0,"hot");
+            typeMap.put(1,"resistance");
+            typeMap.put(2,"shortcircuit");
+            typeMap.put(3,"unconsistency");
+            //String command = "python D:\\Workshop\\untitled4\\early_warning.py " + port +" " + typeMap.get(modelId);
+            String command = "python /home/python/early_warning.py " + port +" " + typeMap.get(modelId);
             //String command = "python D:\\model\\Model" + modelId + ".py " + port;
             System.out.println(command);
             proc = Runtime.getRuntime().exec(command);
